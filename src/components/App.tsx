665f03e28fbe50e5f8f754112c6ef622f1647f9f
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-25 15:47:54 +0100

    Header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,10 @@ import { Header } from './Header'
 
 function App() {
   return (
-    <div className="App h-screen w-screen bg-headerOutline">
-      <Header />
-      <p>test</p>
+    <div className="App flex h-screen w-screen flex-col bg-headerOutline p-8">
+      <div className="flex w-full justify-center">
+        <Header score={12} />
+      </div>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { Logo } from '../public/logo.svg'
+import Logo from '../public/logo.svg'
 
-export const Header = () => (
-  <div className="rounded-lg border border-headerOutline p-4">
-    {Logo}
-    <p>Header</p>
+export const Header = ({ score }: { score: number }) => (
+  <div className="flex items-center justify-between rounded-md border-2 border-headerOutline p-3.5 pl-6 desktop:w-2/3 desktop:rounded-lg">
+    <img
+      src={Logo}
+      alt="Rock Paper Scissors Logo"
+      className="h-12 desktop:h-24"
+    />
+    <div className="flex h-16 w-16 flex-col justify-center rounded-md bg-white text-center desktop:h-28 desktop:w-36 desktop:rounded-lg">
+      <p className="text-[9px] leading-none text-score desktop:text-base">
+        SCORE
+      </p>
+      <p className="text-[28px] font-bold leading-none text-dark desktop:text-5xl">
+        {score}
+      </p>
+    </div>
   </div>
 )
