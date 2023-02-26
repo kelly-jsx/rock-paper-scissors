f2e6b3096c09270a57e16b34de7541fcc509e319
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-26 16:02:31 +0100

    simple game with computer. showing results in console

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -4,7 +4,11 @@ import Scissors from '../../public/icon-scissors.svg'
 import Rock from '../../public/icon-rock.svg'
 import ActionsBg from '../../public/bg-triangle.svg'
 
-export const Actions = () => (
+type Props = {
+  handleChoice: (choice: string) => void
+}
+
+export const Actions = ({ handleChoice }: Props) => (
   <div
     className="actions grid grid-cols-2 justify-items-center gap-20"
     style={{
@@ -13,18 +17,27 @@ export const Actions = () => (
       backgroundPosition: 'center'
     }}
   >
-    <div className="paper grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-paper-primary to-paper-secondary lg:h-48 lg:w-48">
+    <div
+      className="paper grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-paper-primary to-paper-secondary lg:h-48 lg:w-48"
+      onClick={() => handleChoice('paper')}
+    >
       <ActionButton>
         <img src={Paper} alt="" />
       </ActionButton>
     </div>
-    <div className="scissors grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-scissors-primary to-scissors-secondary lg:h-48 lg:w-48">
+    <div
+      className="scissors grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-scissors-primary to-scissors-secondary lg:h-48 lg:w-48"
+      onClick={() => handleChoice('scissors')}
+    >
       <ActionButton>
         <img src={Scissors} alt="" />
       </ActionButton>
     </div>
     <div className="rock col-span-2 rounded-full">
-      <div className="grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-rock-primary to-rock-secondary lg:h-48 lg:w-48">
+      <div
+        className="grid h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-gradient-to-b from-rock-primary to-rock-secondary lg:h-48 lg:w-48"
+        onClick={() => handleChoice('rock')}
+      >
         <ActionButton>
           <img src={Rock} alt="" />
         </ActionButton>
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,6 +8,23 @@ import { RulesModal } from './RulesModal'
 function App() {
   const [showRulesModal, setShowRulesModal] = useState(false)
 
+  const [choice, setChoice] = useState('')
+  const [computerChoice, setComputerChoice] = useState('')
+  const [score, setScore] = useState(0)
+
+  const computerPick = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(computerChoice)
+    console.log('computer choice: ' + computerChoice)
+  }
+
+  const handleChoice = (choice: string) => {
+    setChoice(choice)
+    console.log(choice)
+    computerPick()
+  }
+
   return (
     <>
       <div className="App flex h-screen w-screen flex-col bg-headerOutline p-8">
@@ -15,7 +32,7 @@ function App() {
           <Header score={12} />
         </div>
         <div className="flex h-full w-full items-center justify-center">
-          <Actions />
+          <Actions handleChoice={handleChoice} />
         </div>
         <button
           className="w-max self-center rounded-md border px-12 py-2 text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark lg:self-end"
