8356129ab19309a6b5a711ea752699a6bd1ce2d2
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-27 12:17:58 +0100

    Results screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import '../style.scss'
 import { Actions } from './Actions/Actions'
+import { GameResult } from './GameResult'
 
 import { Header } from './Header'
 import { RulesModal } from './RulesModal'
@@ -12,6 +13,8 @@ function App() {
   const [computerChoice, setComputerChoice] = useState('')
   const [playerWin, setPlayerWin] = useState('')
   const [score, setScore] = useState(0)
+
+  const [showResults, setShowResults] = useState(false)
 
   useEffect(() => {
     winResult()
@@ -58,14 +61,15 @@ function App() {
       <div className="App flex h-screen w-screen flex-col bg-headerOutline p-8">
         <div className="flex w-full justify-center">
           <Header score={score} />
-          <div className="flex flex-col text-white">
+          {/* <div className="flex flex-col text-white">
             <p>You: {choice}</p>
             <p>CPU: {computerChoice}</p>
             <p>{playerWin}</p>
-          </div>
+          </div> */}
         </div>
         <div className="flex h-full w-full items-center justify-center">
-          <Actions handleChoice={handleChoice} />
+          {/* <Actions handleChoice={handleChoice} /> */}
+          <GameResult choice="paper" computerChoice="scissors" />
         </div>
         <button
           className="w-max self-center rounded-md border px-12 py-2 text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark lg:self-end"
--- src/components/GameResult.tsx
+++ src/components/GameResult.tsx
@@ -1,7 +1,92 @@
-export const GameResult = () => {
+import { ActionButton } from './Actions/ActionButton'
+import Paper from './../public/icon-paper.svg'
+import Scissors from './../public/icon-scissors.svg'
+import Rock from './../public/icon-rock.svg'
+
+export const GameResult = ({ choice, computerChoice }) => {
   return (
-    <div>
-      <h1>Game Result</h1>
+    <div className="flex flex-col items-center gap-20 uppercase text-white">
+      <div className="flex gap-8 lg:gap-14">
+        {/* your pick */}
+        <div className="flex flex-col gap-7">
+          {choice === 'paper' ? (
+            <div className="paper grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-paper-primary to-paper-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Paper} alt="" />
+              </div>
+            </div>
+          ) : choice === 'rock' ? (
+            <div className="rock grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-rock-primary to-rock-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Rock} alt="" />
+              </div>
+            </div>
+          ) : (
+            <div className="scissors grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-scissors-primary to-scissors-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Scissors} alt="" />
+              </div>
+            </div>
+          )}
+          <p className="text-center">you picked</p>
+        </div>
+        {/*  */}
+        <div className="hidden flex-col items-center justify-center gap-7 lg:flex">
+          <p className="text-5xl font-bold">You lose</p>
+          <div className="flex">
+            <button className="w-max rounded-lg bg-white px-16 py-3 uppercase text-dark hover:text-rock-primary">
+              play again
+            </button>
+          </div>
+        </div>
+        {/* computer pick */}
+        <div className="flex flex-col gap-7">
+          {computerChoice === 'paper' ? (
+            <div className="paper grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-paper-primary to-paper-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Paper} alt="" />
+              </div>
+            </div>
+          ) : computerChoice === 'rock' ? (
+            <div className="rock grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-rock-primary to-rock-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Rock} alt="" />
+              </div>
+            </div>
+          ) : (
+            <div className="scissors grid h-32 w-32 items-center justify-center rounded-full bg-gradient-to-b from-scissors-primary to-scissors-secondary lg:h-48 lg:w-48">
+              <div
+                className={`action-button relative grid h-24 w-24 place-items-center rounded-full bg-white lg:h-36 lg:w-36`}
+              >
+                <img src={Scissors} alt="" />
+              </div>
+            </div>
+          )}
+          <p className="text-center">the house picked</p>
+        </div>
+      </div>
+      {/* actions */}
+      <div className="flex h-full flex-col items-center gap-7 lg:hidden">
+        <p className="text-5xl font-bold">You lose</p>
+        <div className="flex">
+          <button
+            className="w-max rounded-lg bg-white px-16 py-3 uppercase text-dark hover:text-rock-primary"
+            // onClick={() => handlePlayAgain}
+          >
+            play again
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
