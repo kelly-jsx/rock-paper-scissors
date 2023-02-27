bcbf479ce6a3cb6f81d67ed7adf88c0820649d2a
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-27 13:36:17 +0100

    updating score in state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,50 +10,18 @@ function App() {
   const [showRulesModal, setShowRulesModal] = useState(false)
 
   const [choice, setChoice] = useState('')
-  const [computerChoice, setComputerChoice] = useState('')
-  const [playerWin, setPlayerWin] = useState('')
   const [score, setScore] = useState(0)
 
   const [showResults, setShowResults] = useState(false)
 
-  useEffect(() => {
-    winResult()
-  }, [choice, computerChoice])
-
   const handleChoice = (choice: string) => {
     setChoice(choice)
-    computerPick()
+    setShowResults(true)
+    // computerPick()
   }
 
-  const computerPick = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
-    setComputerChoice(computerChoice)
-    console.log('computer choice: ' + computerChoice)
-  }
-
-  const winResult = () => {
-    if (choice === 'paper' && computerChoice === 'scissors') {
-      setPlayerWin('You lose')
-      setScore(score - 1)
-    } else if (choice === 'paper' && computerChoice === 'rock') {
-      setPlayerWin('You win')
-      setScore(score + 1)
-    } else if (choice === 'scissors' && computerChoice === 'paper') {
-      setPlayerWin('You win')
-      setScore(score + 1)
-    } else if (choice === 'scissors' && computerChoice === 'rock') {
-      setPlayerWin('You lose')
-      setScore(score - 1)
-    } else if (choice === 'rock' && computerChoice === 'paper') {
-      setPlayerWin('You lose')
-      setScore(score - 1)
-    } else if (choice === 'rock' && computerChoice === 'scissors') {
-      setPlayerWin('You win')
-      setScore(score + 1)
-    } else {
-      setPlayerWin('Draw')
-    }
+  const handleChangeScore = (updatedScore: number) => {
+    setScore(updatedScore)
   }
 
   return (
@@ -61,15 +29,18 @@ function App() {
       <div className="App flex h-screen w-screen flex-col bg-headerOutline p-8">
         <div className="flex w-full justify-center">
           <Header score={score} />
-          {/* <div className="flex flex-col text-white">
-            <p>You: {choice}</p>
-            <p>CPU: {computerChoice}</p>
-            <p>{playerWin}</p>
-          </div> */}
         </div>
         <div className="flex h-full w-full items-center justify-center">
-          {/* <Actions handleChoice={handleChoice} /> */}
-          <GameResult choice="paper" computerChoice="scissors" />
+          {showResults ? (
+            <GameResult
+              choice={choice}
+              handlePlayAgain={() => setShowResults(false)}
+              handleChangeScore={handleChangeScore}
+              score={score}
+            />
+          ) : (
+            <Actions handleChoice={handleChoice} />
+          )}
         </div>
         <button
           className="w-max self-center rounded-md border px-12 py-2 text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark lg:self-end"
--- src/components/GameResult.tsx
+++ src/components/GameResult.tsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from 'react'
 import { ActionButton } from './Actions/ActionButton'
 import Paper from './../public/icon-paper.svg'
 import Scissors from './../public/icon-scissors.svg'
 import Rock from './../public/icon-rock.svg'
 
-export const GameResult = ({ choice, computerChoice }) => {
+type Props = {
+  choice: string
+  score: number
+  handlePlayAgain: () => void
+  handleChangeScore: (score: number) => void
+}
+
+export const GameResult = ({
+  choice,
+  score,
+  handlePlayAgain,
+  handleChangeScore
+}: Props) => {
+  const [computerChoice, setComputerChoice] = useState('')
+  const [playerWin, setPlayerWin] = useState('')
+
+  useEffect(() => {
+    handleResult(choice)
+  }, [choice])
+
+  const handleResult = (playerChoice: string) => {
+    const computerChoice = computerPick()
+    setComputerChoice(computerChoice)
+    winResult(playerChoice, computerChoice)
+  }
+
+  const computerPick = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    return computerChoice
+  }
+
+  const winResult = (playerChoice: string, computerChoice: string) => {
+    if (playerChoice === 'paper' && computerChoice === 'scissors') {
+      setPlayerWin('You lose')
+      handleChangeScore(score - 1)
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+      setPlayerWin('You win')
+      handleChangeScore(score + 1)
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+      setPlayerWin('You win')
+      handleChangeScore(score + 1)
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+      setPlayerWin('You lose')
+      handleChangeScore(score - 1)
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+      setPlayerWin('You lose')
+      handleChangeScore(score - 1)
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+      setPlayerWin('You win')
+      handleChangeScore(score + 1)
+    } else {
+      setPlayerWin('Draw')
+    }
+  }
+
   return (
     <div className="flex flex-col items-center gap-20 uppercase text-white">
       <div className="flex gap-8 lg:gap-14">
@@ -38,9 +94,12 @@ export const GameResult = ({ choice, computerChoice }) => {
         </div>
         {/*  */}
         <div className="hidden flex-col items-center justify-center gap-7 lg:flex">
-          <p className="text-5xl font-bold">You lose</p>
+          <p className="text-5xl font-bold">{playerWin}</p>
           <div className="flex">
-            <button className="w-max rounded-lg bg-white px-16 py-3 uppercase text-dark hover:text-rock-primary">
+            <button
+              className="w-max rounded-lg bg-white px-16 py-3 uppercase text-dark hover:text-rock-primary"
+              onClick={handlePlayAgain}
+            >
               play again
             </button>
           </div>
@@ -77,11 +136,11 @@ export const GameResult = ({ choice, computerChoice }) => {
       </div>
       {/* actions */}
       <div className="flex h-full flex-col items-center gap-7 lg:hidden">
-        <p className="text-5xl font-bold">You lose</p>
+        <p className="text-5xl font-bold">{playerWin}</p>
         <div className="flex">
           <button
             className="w-max rounded-lg bg-white px-16 py-3 uppercase text-dark hover:text-rock-primary"
-            // onClick={() => handlePlayAgain}
+            onClick={handlePlayAgain}
           >
             play again
           </button>
